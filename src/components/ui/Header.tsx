efb83ee8bc6e08602bc4194982fc7c838cbb6b4f
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-07-30 12:24:35 +0300

    Add src/components/ui/Header.tsx

---
+++ src/components/ui/Header.tsx
@@ -0,0 +1,7 @@
+const Header = () => (
+  <header className="bg-blue-600 text-white py-4">
+    <h1 className="text-center text-3xl font-bold">Note Taking App</h1>
+  </header>
+);
+
+export default Header;
